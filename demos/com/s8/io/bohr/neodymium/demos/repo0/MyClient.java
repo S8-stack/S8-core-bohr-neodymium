41e339553a9a2b5424c3bbcc370a1164b707a1ef
Pierre Convert <pierreconvert@macbook-pro-6.lan>
2023-04-03 18:31:00 +0200

    update

---
+++ demos/com/s8/io/bohr/neodymium/demos/repo0/MyClient.java
@@ -0,0 +1,18 @@
+package com.s8.io.bohr.neodymium.demos.repo0;
+
+import com.s8.io.bohr.lithium.object.LiS8Object;
+
+/**
+ * 
+ * @author pierreconvert
+ *
+ */
+public class MyClient extends LiS8Object {
+
+	
+	public MyClient() {
+		super();
+	}
+
+	
+}
