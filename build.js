b1b1bcd88e0e39deedb4f2d0fda9fde0b219ad89
Pierre Convert <pierreconvert@192.168.1.14>
2025-01-26 17:10:47 +0100

    Updating build.js

--- build.js
+++ build.js
@@ -1,5 +1,10 @@
 const ROOT = build({
 	module: "com.s8.core.bohr.neodymium",
-	dependencies: [ "S8-api", "S8-core-io-bytes", "S8-core-bohr-atom"],
-	target: "S8-core-bohr-neodymium",
-});
+	dependencies: [
+		"S8-api",
+		"S8-core-io-bytes",
+		"S8-core-io-JSON",
+		"S8-core-bohr-atom"
+	],
+	target: "S8-core-bohr-neodymium"
+});	
